ab0bfbd91e412115026775c991fdbe75efc9aa3e
kalebetavares <kalebe.tavares@gmail.com>
2022-03-06 19:33:35 -0300

    Fim do curso

--- src/StreamAPI/ExerciciosStreamAPI.java
+++ src/StreamAPI/ExerciciosStreamAPI.java
@@ -18,12 +18,34 @@ public class ExerciciosStreamAPI {
 		numerosAleatorios.stream().limit(5)
 		.collect(Collectors.toSet()).forEach(System.out::println);
 		
-		System.out.println("Transforme esta lista de String em uma lista de Inteiros: ");
+		/*System.out.println("Transforme esta lista de String em uma lista de Inteiros: ");
 		List<Integer> collectList = numerosAleatorios.stream()
 				.map(Integer::parseInt)
 				.collect(Collectors.toList());
 		
-		System.out.println(collectList);
+		System.out.println(collectList);*/
+		
+		System.out.println("Pegue os números pares e maiores que 2 e coloque em uma lista: ");
+		numerosAleatorios.stream().map(Integer::parseInt)
+		.filter(i -> i % 2 == 0 && i > 2)
+		.collect(Collectors.toList())
+		.forEach(System.out::println);
+		
+		System.out.println("Mostre a média dos números: ");
+		numerosAleatorios.stream().mapToInt(Integer::parseInt)
+				.average().ifPresent(System.out::println);
+	
+		
+		System.out.println("Remova os valores ímpares: ");
+		List<Integer> numerosAleatoriosInteger = numerosAleatorios.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+		
+		numerosAleatoriosInteger.removeIf(integer -> integer % 2 != 0);
+		System.out.println(numerosAleatoriosInteger);
+		
+	
+	
 	}
 
 }
